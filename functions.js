acaa1b846ce101938fb98404e078007b3abda5bc
Craig Taub <craigtaub@gmail.com>
2013-12-02 22:36:37 +0000

    removed some useless comments

--- functions.js
+++ functions.js
@@ -1,28 +1,24 @@
 var functions = {
     runComparison : function(item, err, gm, screens) {
-      //compare 
+      //compare Master with diff 
       gm.compare(screens+item+".jpg", screens+item+".diff.jpg", function (err, isEqual, equality, raw) {
         if (err) throw err;
-
         if (equality > 0) {
-          //images not the same
-          
+          //images not the same, FAIL.
           console.log("FAILED Diff bp:", item);
           console.log("FAILED equality rate:", equality, "%");
-
           //create .fail
           functions.createFail(item, gm, screens);
-
         } else {
-          //no change
+          //no change, PASS.
           console.log("PASSED Diff bp:", item);
         }
       });
     },
     createFail : function(item, gm, screens) {
         var options = {
-          highlightColor: 'yellow', // optional. Defaults to red
-          file: screens+item+'.fail.jpg' // required
+          highlightColor: 'yellow',
+          file: screens+item+'.fail.jpg'
         };
         gm.compare(screens+item+".jpg", screens+item+".diff.jpg", options, function (err) {
           if (err) throw err;
