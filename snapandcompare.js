98c1a2d62ba71e9858603767a767fa073f901132
Craig Taub <craigtaub@gmail.com>
2013-12-02 22:35:28 +0000

    removing some useless comments

--- snapandcompare.js
+++ snapandcompare.js
@@ -1,13 +1,13 @@
 //node modules
-var config = require('./config'), //config
-    functions = require('./functions'), //functions
-    webshot = require('webshot'), //Screen shots
+var config = require('./config'),
+    functions = require('./functions'),
+    webshot = require('webshot'),
     gm = require('gm'), //GraphicMagick
     fs = require('fs'); //filesystem
 
 //foreach breakpoint
 config.breakpoints.forEach(function(item) {
-    //set bp options
+    //set breakpoint options
     var options = {
       screenSize: {
         width: item, height: 1080
@@ -19,13 +19,13 @@ config.breakpoints.forEach(function(item) {
         if (exists) {
             //master exists. Create snapshot .diff
             webshot(config.diffUrl, config.screens+item+'.diff.jpg', options,  function(err) {
-                //screenshot now saved .diff...will overwrite if necessary
+                //screenshot now saved .diff...will overwrite if necessary.
                 functions.runComparison(item, err, gm, config.screens);
             });
         } else {
-            //master doesnt exist. Create snapshot master
+            //master doesnt exist. Create snapshot master.
             webshot(config.masterUrl, config.screens+item+'.jpg', options,  function(err) {
-               //master screenshot now saved
+               //master screenshot now stored.
                 console.log("MASTER Stored bp:", item);
             });
         }
